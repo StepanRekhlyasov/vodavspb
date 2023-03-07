e863158eb74e78f803c3136fb00bf4fc91bb7261
Stepan <srekhlyasov@gmail.com>
2023-03-07 16:29:51 +0300

    some clearing

--- helpers.ts
+++ helpers.ts
@@ -24,58 +24,58 @@ export function countInCart(id : Number, cart : Cart[]){
         return 0
     }
 }
-export function addOne({item, cart} : {
-    item: Product,
-    cart: Cart[]
-}){
-    const in_cart = cart.find((row)=>{
-        return row.ID == item.ID
-    })
-    if(in_cart && in_cart.count>=item.qty){
-        return in_cart.count + 1
-    } else {
-        return item.qty?item.qty:1
-    }
-}
+// export function addOne({item, cart} : {
+//     item: Product,
+//     cart: Cart[]
+// }){
+//     const in_cart = cart.find((row)=>{
+//         return row.ID == item.ID
+//     })
+//     if(in_cart && in_cart.count>=item.qty){
+//         return in_cart.count + 1
+//     } else {
+//         return item.qty?item.qty:1
+//     }
+// }
 
-export function removeOne({item, cart} : {
-    item: Product,
-    cart: Cart[]
-}){
-    const in_cart = cart.find((row)=>{
-        return row.ID == item.ID
-    })
-    if(in_cart && in_cart.count>item.qty){
-        return in_cart.count - 1
-    } else {
-        return 0
-    }
-}
+// export function removeOne({item, cart} : {
+//     item: Product,
+//     cart: Cart[]
+// }){
+//     const in_cart = cart.find((row)=>{
+//         return row.ID == item.ID
+//     })
+//     if(in_cart && in_cart.count>item.qty){
+//         return in_cart.count - 1
+//     } else {
+//         return 0
+//     }
+// }
 
-export function countTotalCart(cart : Cart[]){
-    let count = cart.reduce((acc, row)=>{
-        return row.count + acc
-    }, 0)
-    return count
-}
+// export function countTotalCart(cart : Cart[]){
+//     let count = cart.reduce((acc, row)=>{
+//         return row.count + acc
+//     }, 0)
+//     return count
+// }
 
-export function countCostCart(cart : Cart[], products : Product[]){
-    let cost = cart.reduce((acc : any, row : any)=>{
-        return row.count * getProductPrice(row.ID, products) + acc
-    }, 0)
-    return cost
-}
+// export function countCostCart(cart : Cart[], products : Product[]){
+//     let cost = cart.reduce((acc : any, row : any)=>{
+//         return row.count * getProductPrice(row.ID, products) + acc
+//     }, 0)
+//     return cost
+// }
 
-export function getProductPrice(id: Number, products : Product[]){
-    const result = products.find((row)=>{
-        return row.ID == id
-    })
-    if(result){
-        return result.price
-    } else {
-        return 0
-    }
-}
+// export function getProductPrice(id: Number, products : Product[]){
+//     const result = products.find((row)=>{
+//         return row.ID == id
+//     })
+//     if(result){
+//         return result.price
+//     } else {
+//         return 0
+//     }
+// }
 
 export function geocodeResultToStreet(json : Geocoder.GeocoderResponse) {
     let result = json.results.find((row)=>{
